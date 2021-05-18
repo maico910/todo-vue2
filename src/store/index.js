58c3f2eadb079e0ccde3c1afc9f2f991b73a5ed0
Michael Wu <maico910@gmail.com>
2021-05-18 11:18:09 -0300

    vuex: init module-todo

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-// import example from './module-example'
+import todo from './module-todo'
 
 Vue.use(Vuex)
 
@@ -17,7 +17,7 @@ Vue.use(Vuex)
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
-      // example
+      todo
     },
 
     // enable strict mode (adds overhead!)
