00341e383e63e27c340e36f0170012e63b5dd78d
Michael Wu <maico910@gmail.com>
2021-05-20 10:44:01 -0300

    feat: show list

---
+++ src/components/Todo/CardTodo.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-item
+    v-ripple
+    clickable
+    class="q-card column"
+    style="width: 250px"
+  >
+    <q-item-label :class="{'list-completed': currentTodos.length && currentTodos.every(todo => todo.completed)}" class="flex q-py-sm q-px-none justify-between items-center" header>
+      {{ list.name }}
+
+  </q-item>
+</template>
+
+<script>
+export default {
+  props: {
+
+    list: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      currentTodos: []
+    }
+  },
+
+  computed: {
+    todos() {
+      return this.list.todos
+    },
+
+    todosValue() {
+      return this.list.todos.filter(todo => todo.completed).map(completedTodo => completedTodo.name)
+    },
+
+    todosOptions() {
+      return this.list.todos.map((todo) => {
+        return {
+          label: todo.name,
+          value: todo.name
+        }
+      })
+    }
+  },
+
+  mounted () {
+    this.currentTodos = JSON.parse(JSON.stringify(this.todos))
+  },
+
+  methods: {
+    updateTodo(targetTodo) {
+      this.currentTodos.find(todo => todo.name === targetTodo.name).completed = targetTodo.completed
+    }
+  }
+}
+</script>
---
+++ src/components/Todo/ListTodo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="list-todo" class="row no-wrap">
+    <template v-if="lists.length">
+      <CardTodo v-for="(list, index) in lists" :key="index" :list="list" />
+    </template>
+    <template v-else>
+      <q-card class="q-pa-md cursor-pointer">
+        Nenhuma lista criada. Adicione pelo menu.
+      </q-card>
+    </template>
+  </div>
+</template>
+
+<script>
+import CardTodo from './CardTodo'
+export default {
+  components: { CardTodo },
+  props: {
+    lists: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+  #list-todo {
+    align-items: baseline;
+
+    > * {
+      margin: 16px;
+    }
+
+    .q-card:hover {
+      transition: 0.3s all;
+      background-color: #E4E4E4;
+    }
+  }
+
+</style>
